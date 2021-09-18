94841ba34da6841f32d3850572721b99aa94fc80
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-09-18 12:44:00 +0300

    added UserController

---
+++ CarShop/CarShop.Web/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿namespace CarShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return null;
+        }
+    }
+}
