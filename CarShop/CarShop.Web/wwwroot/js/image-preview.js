d5a3ad2758739c3b5c9c3905bea8cb2c062e148f
Martin Ali <marto7395@gmail.com>
2021-09-18 13:25:32 +0300

    Added _CreationScriptsPartial and needed JS

---
+++ CarShop/CarShop.Web/wwwroot/js/image-preview.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    function setupPostPreview() {
+        const preview = document.getElementById('image-preview');
+        const input = document.getElementById('image-upload');
+
+        if (input && preview) {
+            input.addEventListener('change', (event) => {
+                preview.src = URL.createObjectURL(event.target.files[0]);
+            });
+        }
+    }
+
+    window.addEventListener('load', setupPostPreview)
+})();
